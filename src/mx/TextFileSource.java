393cf6457a24a769659bb3568af0728b75a1813f
masterix <masterix.zone@gmail.com>
2017-05-28 22:10:30 +0200

    TextFileSource refactoring

--- src/mx/TextFileSource.java
+++ src/mx/TextFileSource.java
@@ -1,44 +1,41 @@
 package mx;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created by masterix on 23.11.2016.
- */
-public class TextFileSource implements ISource {
+public class TextFileSource implements SourceInterface {
 
     protected ArrayList<Domain> domains;
 
     protected String filename;
 
-    public TextFileSource() {
+    public TextFileSource(String inputFilename) {
         domains = new ArrayList<>();
+        filename = inputFilename;
     }
 
     @Override
-    public ArrayList<Domain> getDomainList() {
+    /**
+     * @throws SourceNotFoundException
+     */
+    public ArrayList<Domain> getDomainList() throws SourceNotFoundException {
+        domains.clear();
+        try{
+            readDomainsFromFile();
+        }
+        catch(IOException ex){
+            throw new SourceNotFoundException(ex.getMessage());
+        }
+
         return domains;
     }
 
-    /**
-     * New filename resets domains' list
-     * @param filename
-     * @throws IOException
-     */
-    public void setFilenameAndRead(String filename) throws IOException {
-        this.filename = filename;
-        domains.clear();
-        readDomainsFromCsv();
-    }
-
-    protected void readDomainsFromCsv() throws IOException {
+    private void readDomainsFromFile() throws IOException {
         FileReader fr = new FileReader(filename);
         BufferedReader br = new BufferedReader(fr);
-        String url = null;
+        String url;
         while ((url = br.readLine()) != null) {
             domains.add(new Domain(url));
         }
